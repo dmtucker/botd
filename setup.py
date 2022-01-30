f84bd84fcf8d9a9327f864a7e944e88f064a0e0b
David Tucker <david@tucker.name>
2022-01-30 10:39:50 -0800

    Use twisted's tls extra

--- setup.py
+++ setup.py
@@ -44,9 +44,7 @@ if __name__ == "__main__":
         python_requires=">= 3.9",
         install_requires=[
             "passlib >= 1.7",
-            "pyOpenSSL",
-            "service_identity",
-            "twisted",
+            "twisted[tls] >= 22.1",
         ],
         entry_points=ENTRY_POINTS,
         classifiers=[
